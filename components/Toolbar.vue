52a2042ea209e1f96fc038bdf9ba4ceb413c7afc
henry3556108 <36980686+henry3556108@users.noreply.github.com>
2019-04-08 14:38:09 +0800

    Add Toolbar

---
+++ components/Toolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="toolbar">
+    <div class="toolbar__logo" src="#" />
+    <buttom class="toolbar__homepage" src="#" />
+    <button class="toolbar__about_me" src="#" />
+  </div>
+</template>
+
+<style lang="sass">
+  .toolbar
+    display: flex
+    align-items: center
+    justify-content: space-between
+</style>
