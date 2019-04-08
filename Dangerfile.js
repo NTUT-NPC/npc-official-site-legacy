e57d04832facdbc20e3959860634746f693245bd
Jamfly <cocoredju222@gmail.com>
2019-04-08 21:00:42 +0800

    Fix lint

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
+const danger = require('danger')
 const _ = require('lodash')
 const validateMessage = require('validate-commit-msg')
-
 // simple regex matcher to detect usage of helper function and its type signature
 const hotMatch = /\bhot\(/gi
 const hotSignatureMatch = /\bimport \{.*?hot.*?\} from '.*?\/helpers\/marble-testing'/gi
@@ -15,8 +15,8 @@ let errorCount = 0
 // Warn when PR size is large
 const bigPRThreshold = 600
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR (' + ++errorCount + ')')
-  markdown(
+  danger.warn(':exclamation: Big PR (' + ++errorCount + ')')
+  danger.markdown(
     '> (' +
       errorCount +
       ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.'
@@ -43,7 +43,7 @@ const testFilesIncludeExclusion = modifiedSpecFiles.reduce(function (
 [])
 
 if (testFilesIncludeExclusion.length > 0) {
-  fail('an `only` was left in tests (' + testFilesIncludeExclusion + ')')
+  danger.fail('an `only` was left in tests (' + testFilesIncludeExclusion + ')')
 }
 
 // Check test cases missing type signature import for test marble helper functions
@@ -63,14 +63,14 @@ const testFilesMissingTypes = modifiedSpecFiles.reduce(function (acc, value) {
 }, [])
 
 if (testFilesMissingTypes.length > 0) {
-  fail(
+  danger.fail(
     'missing type definition import in tests (' +
       testFilesMissingTypes +
       ') (' +
       ++errorCount +
       ')'
   )
-  markdown(
+  danger.markdown(
     '> (' +
       errorCount +
       ') : It seems updated test cases uses test scheduler interface `hot`, `cold` but miss to import type signature for those.'
@@ -84,12 +84,12 @@ const messageConventionValid = danger.git.commits.reduce(function (acc, value) {
 }, true)
 
 if (!messageConventionValid) {
-  warn(
+  danger.warn(
     'commit message does not follows conventional change log (' +
       ++errorCount +
       ')'
   )
-  markdown(
+  danger.markdown(
     '> (' +
       errorCount +
       ') : RxJS uses conventional change log to generate changelog automatically. It seems some of commit messages are not following those, please check [contributing guideline](https://github.com/ReactiveX/rxjs/blob/master/CONTRIBUTING.md#commit-message-format) and update commit messages.'
