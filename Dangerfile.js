c322c5bbed006c1fa38c56e6a33025798456dd62
Jamfly <cocoredju222@gmail.com>
2019-04-08 23:35:20 +0800

    Update Dangerfile.js

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,7 +1,8 @@
-import { danger, fail, markdown, warn } from 'danger'
 const fs = require('fs')
 const path = require('path')
+const { danger, fail, warn, markdown, message } = require('danger')
 const _ = require('lodash')
+const includes = require('lodash.includes')
 
 // simple regex matcher to detect usage of helper function and its type signature
 const hotMatch = /\bhot\(/gi
@@ -75,4 +76,45 @@ if (testFilesMissingTypes.length > 0) {
       errorCount +
       ') : It seems updated test cases uses test scheduler interface `hot`, `cold` but miss to import type signature for those.'
   )
+}
+
+// Warns if there are changes to package.json, and tags the team.
+const packageChanged = includes(danger.git.modified_files, 'package.json')
+if (packageChanged) {
+  const title = ':lock: package.json'
+  const idea =
+    'Changes were made to package.json. ' +
+    'This will require a manual import by a Facebook employee.'
+  warn(`${title} - <i>${idea}</i>`)
+}
+
+// Provides advice if a summary section is missing, or body is too short
+const includesSummary =
+  danger.github.pr.body &&
+  danger.github.pr.body.toLowerCase().includes('## summary')
+if (!danger.github.pr.body || danger.github.pr.body.length < 50) {
+  fail(':grey_question: This pull request needs a description.')
+} else if (!includesSummary) {
+  const title = ':clipboard: Missing Summary'
+  const idea =
+    'Can you add a Summary? ' +
+    'To do so, add a "## Summary" section to your PR description. ' +
+    'This is a good place to explain the motivation for making this change.'
+  message(`${title} - <i>${idea}</i>`)
+}
+
+// Warns if the PR is opened against stable, as commits need to be cherry picked and tagged by a release maintainer.
+// Fails if the PR is opened against anything other than `master` or `-stable`.
+const isMergeRefMaster = danger.github.pr.base.ref === 'master'
+const isMergeRefStable = danger.github.pr.base.ref.indexOf('-stable') !== -1
+if (!isMergeRefMaster && isMergeRefStable) {
+  const title = ':grey_question: Base Branch'
+  const idea =
+    'The base branch for this PR is something other than `master`. Are you sure you want to merge these changes into a stable release? If you are interested in backporting updates to an older release, the suggested approach is to land those changes on `master` first and then cherry-pick the commits into the branch for that release. The [Releases Guide](https://github.com/facebook/react-native/blob/master/Releases.md) has more information.'
+  warn(`${title} - <i>${idea}</i>`)
+} else if (!isMergeRefMaster && !isMergeRefStable) {
+  const title = ':exclamation: Base Branch'
+  const idea =
+    'The base branch for this PR is something other than `master`. [Are you sure you want to target something other than the `master` branch?](http://facebook.github.io/react-native/docs/contributing.html#pull-requests)'
+  fail(`${title} - <i>${idea}</i>`)
 }
