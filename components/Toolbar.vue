eb4b9a1eb68526c99b08e539d8a839a9b18fba55
henry3556108 <36980686+henry3556108@users.noreply.github.com>
2019-04-11 13:42:26 +0800

    Modify toolbar style

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -3,16 +3,23 @@
     <img class="toolbar__logo" src="#">
     <div class="toolbar__link">
       <button class="toolbar__link__homepage" src="#">
-        homepage
+        首頁
       </button>
       <button class="toolbar__link__about_me" src="#">
-        aboutme
+        關於我們
+      </button>
+      <button class="toolbar__link__about_me" src="#">
+        學習資源
+      </button>
+      <button class="toolbar__link__about_me" src="#">
+        登入
       </button>
     </div>
   </div>
 </template>
 
 <style lang="sass">
+
   button
     background-color: #0c1214
     border: none
@@ -20,7 +27,13 @@
     padding: 15px 32px
     text-align: center
     font-size: 16px
+    border-bottom: solid 2px #0c1214
+    transition: 0.5s
+  button:hover
+      cursor: pointer
+      border-bottom: solid 2px white
   .toolbar
+    position: relative
     display: flex
     height: 10%
     width: 100%
@@ -28,6 +41,8 @@
     .toolbar__logo
       color: white
     .toolbar__link
+      position: relative
+      left: 60%
       margin-left: 50px
       display: flex
       align-items: center
