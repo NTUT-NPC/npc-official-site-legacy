31a5f664a2fac38e8f947a9fd06e0e81cc3021d4
henry3556108 <36980686+henry3556108@users.noreply.github.com>
2019-05-30 21:25:38 +0800

    add hamburger menu

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,46 +1,51 @@
 <template>
-  <div class="contaner">
-    <img
-      class="contaner__logo"
-      src="../static/logo_normal_word.png"
-    >
-    <div class="contaner__toolbar">
-      <button
-        class="toolbar__link__homepage"
-        src="#"
+  <div>
+    <div class="contaner hidden-sm-and-down">
+      <img
+        class="contaner__logo"
+        src="../static/logo_normal_word.png"
       >
-        <nuxt-link to="/">
-          <h3>
-            首頁
-          </h3>
-        </nuxt-link>
-      </button>
-      <button
-        src="#"
-      >
-        <nuxt-link to="/activities/">
-          <h3>
-            活動
-          </h3>
-        </nuxt-link>
-      </button>
-      <button
-        class="toolbar__link__about_me"
-        src="#"
-      >
-        <h3>學習資源</h3>
-      </button>
-      <button
-        class="toolbar__link__about_me"
-        src="#"
-      >
-        <nuxt-link to="/aboutMe/">
-          <h3>
-            關於
-          </h3>
-        </nuxt-link>
-      </button>
+      <div class="contaner__toolbar">
+        <button
+          class="toolbar__link__homepage"
+          src="#"
+        >
+          <nuxt-link to="/">
+            <h3>
+              首頁
+            </h3>
+          </nuxt-link>
+        </button>
+        <button
+          src="#"
+        >
+          <nuxt-link to="/activities/">
+            <h3>
+              活動
+            </h3>
+          </nuxt-link>
+        </button>
+        <button
+          class="toolbar__link__about_me"
+          src="#"
+        >
+          <h3>學習資源</h3>
+        </button>
+        <button
+          class="toolbar__link__about_me"
+          src="#"
+        >
+          <nuxt-link to="/aboutMe/">
+            <h3>
+              關於
+            </h3>
+          </nuxt-link>
+        </button>
+      </div>
     </div>
+    <v-toolbar class="hidden-md-and-up" flat dark color="#0c1214">
+      <v-toolbar-side-icon />
+    </v-toolbar>
   </div>
 </template>
 
@@ -55,7 +60,7 @@
     height: 100px
   .contaner__toolbar
     display: flex
-  button
+  .contaner button
     padding: 15px 32px
     text-align: center
     border-bottom: solid 2px #0c1214
@@ -63,8 +68,8 @@
     outline: none
   a
     text-decoration: none
+  .contaner button:hover
+    cursor: pointer
+    border-bottom: solid 2px white
 
-  button:hover
-      cursor: pointer
-      border-bottom: solid 2px white
 </style>
